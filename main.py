537830458243c98c43691ac35d57b2cb4a458cd5
Sankalp Singh <838sanky@gmail.com>
2023-07-07 11:57:42 -0400

    badri adores headphones

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-imdb_movies = pd.read_csv('imdb_movies.csv', index_col = "names")
+imdb_movies = pd.read_csv('data/imdb_movies.csv', index_col ="names")
 
 imdb_x_vals = imdb_movies[['month_num', 'genre_num', 'budget_x', 'country_num']]
 imdb_y_vals = imdb_movies[['revenue', 'score']]
@@ -15,7 +15,7 @@ regr = linear_model.LinearRegression()
 regr.fit(imdb_x_vals, imdb_y_vals)
 predicted_shit = regr.predict([[3, 1, 75000000, 1]])
 print(predicted_shit)
-# date, score, genre, orig_lang, first budget_x, countrydf
+# date, score, genre, orig_lang, first budget_x, countryasdkl;fawe;krfajl;sdfj badri likes cock
 
 
 
