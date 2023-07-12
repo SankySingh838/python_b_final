3089acedadcf2eafd8b1ac4e44832a5ebd6b1ddb
Sankalp Singh <838sanky@gmail.com>
2023-07-12 11:05:12 -0400

    fixed data

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.tree import DecisionTreeRegressor
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-imdb_movies = pd.read_csv('data/imdb_movies.csv', index_col ="names")
+imdb_movies = pd.read_csv('data/imdb_movies.csv', index_col="names")
 
+# Set the feature names
 imdb_x_vals = imdb_movies[['month_num', 'genre_num', 'budget_x', 'country_num']]
 imdb_y_vals = imdb_movies[['revenue', 'score']]
 
 regr = linear_model.LinearRegression()
 regr.fit(imdb_x_vals, imdb_y_vals)
-predicted_shit = regr.predict([[3, 1, 75000000, 1]])
-print(predicted_shit)
-# date, score, genre, orig_lang, first budget_x, countryasdkl;fawe;krfajl;sdfj badri likes cock
 
 
-
+for index, row in imdb_movies.iterrows():
+    genre_num_movie = row['genre_num']
+    month_num_movie = row['month_num']
+    country_num_movie = row['country_num']
+    budget_x_movie = row['budget_x']
+    predicted_shit = regr.predict([[month_num_movie, genre_num_movie, budget_x_movie, country_num_movie]])
