c1194d0b2ffe3f6f88143020bff0fd085f786ba2
Sankalp Singh <838sanky@gmail.com>
2023-07-13 22:34:54 -0400

    added new_movie_dataa.csv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-imdb_movies = pd.read_csv('data/imdb_movies.csv', index_col="names")
+imdb_movies = pd.read_csv('data/stripped_imdb_movies_data.csv', index_col="names")
 
 # Set the feature names
 feature_names = ['month_num', 'genre_num', 'budget_x', 'country_num']
@@ -15,8 +15,9 @@ regr = LinearRegression()
 regr.fit(imdb_x_vals, imdb_y_vals)
 
 # Example of using the trained model on new data
-#{'month_num': [3, 6, 9], 'genre_num': [2, 1, 4], 'budget_x': [5000000, 10000000, 20000000], 'country_num': [1, 3, 2]}
-new_data = pd.read_csv('data/new_movie_dataa.csv', index_col="names")
+new_data = pd.read_csv('data/new_movie_data.csv', index_col="names")
+new_data = new_data[feature_names]  # Keep only the desired features from the new data
+
 predicted_values = regr.predict(new_data)
 print("Predicted values for new data:")
 print(predicted_values)
