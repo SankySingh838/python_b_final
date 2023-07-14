50346c7a5aaef5059f895f7b5bc4cf4bcb5cc661
Sankalp Singh <838sanky@gmail.com>
2023-07-13 22:37:47 -0400

    finished the project (tentatively)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
 
 imdb_movies = pd.read_csv('data/stripped_imdb_movies_data.csv', index_col="names")
 
@@ -21,3 +23,31 @@ new_data = new_data[feature_names]  # Keep only the desired features from the ne
 predicted_values = regr.predict(new_data)
 print("Predicted values for new data:")
 print(predicted_values)
+
+# Extract the predicted revenue and score from the predicted values
+predicted_revenue = predicted_values[:, 0]
+predicted_score = predicted_values[:, 1]
+
+# Create a list of movie names
+movie_names = new_data.index.values
+
+# Plot the predicted revenue
+plt.figure(figsize=(10, 6))
+plt.bar(movie_names, predicted_revenue)
+plt.xlabel('Movie')
+plt.ylabel('Predicted Revenue')
+plt.title('Predicted Revenue for New Movies')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Plot the predicted score
+plt.figure(figsize=(10, 6))
+plt.bar(movie_names, predicted_score)
+plt.xlabel('Movie')
+plt.ylabel('Predicted Score')
+plt.title('Predicted Score for New Movies')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
