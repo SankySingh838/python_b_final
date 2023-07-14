0867aba150d11abd8c5494feaadb84977b4a22d4
Sankalp Singh <838sanky@gmail.com>
2023-07-13 22:50:49 -0400

    finished the project (tentatively)

--- programs/main.py
+++ programs/main.py
@@ -3,22 +3,26 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-imdb_movies = pd.read_csv('data/stripped_imdb_movies_data.csv', index_col="names")
+# Read the IMDb movies data into a pandas DataFrame
+imdb_movies = pd.read_csv('../data/stripped_imdb_movies_data.csv', index_col="names")
 
-# Set the feature names
+# Set the feature names and target names
 feature_names = ['month_num', 'genre_num', 'budget_x', 'country_num']
 target_names = ['revenue', 'score']
 
+# Extract the feature values (X) and target values (y) from the IMDb movies data
 imdb_x_vals = imdb_movies[feature_names].values
 imdb_y_vals = imdb_movies[target_names].values
 
+# Create a Linear Regression model and fit it to the IMDb movies data
 regr = LinearRegression()
 regr.fit(imdb_x_vals, imdb_y_vals)
 
-# Example of using the trained model on new data
-new_data = pd.read_csv('data/new_movie_data.csv', index_col="names")
+# Load the new movie data for prediction
+new_data = pd.read_csv('../data/new_movie_data.csv', index_col="names")
 new_data = new_data[feature_names]  # Keep only the desired features from the new data
 
+# Predict the target values (revenue and score) for the new movie data using the trained model
 predicted_values = regr.predict(new_data)
 print("Predicted values for new data:")
 print(predicted_values)
@@ -30,7 +34,7 @@ predicted_score = predicted_values[:, 1]
 # Create a list of movie names
 movie_names = new_data.index.values
 
-# Plot the predicted revenue
+# Plot the predicted revenue for the new movies
 plt.figure(figsize=(10, 6))
 plt.bar(movie_names, predicted_revenue)
 plt.xlabel('Movie')
@@ -41,7 +45,7 @@ plt.tight_layout()
 plt.savefig('predicted_revenue.png')  # Save the plot as an image
 plt.show()
 
-# Plot the predicted score
+# Plot the predicted score for the new movies
 plt.figure(figsize=(10, 6))
 plt.bar(movie_names, predicted_score)
 plt.xlabel('Movie')
